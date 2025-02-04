f165a7b59708cf6c2d05871f0af92fce62bba769
william-reges <william.reges1986@gmail.com>
2025-02-04 15:47:46 -0300

    first commit

---
+++ src/main/java/com/example/lab_a01_infra_service_registry/LabA01InfraServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab_a01_infra_service_registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabA01InfraServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabA01InfraServiceRegistryApplication.class, args);
+	}
+
+}
